7242a59a38dbe8fa486d4816bade809832f5110c
Neil <neil.bennett01@gmail.com>
2022-06-06 16:25:46 -0400

    security back end

---
+++ functions/index.js
@@ -0,0 +1,56 @@
+import functions from "firebase-functions";
+import express from "express";
+import cors from "cors";
+import jwt from "jsonwebtoken";
+import mySecretKey from "./secret.js";
+
+const users = [
+  //fake db
+  { id: 1, email: "todd@bocacode.com", password: "abc123" },
+  { id: 2, email: "damian@bocacode.com", password: "def456" },
+  { id: 3, email: "vitoria@bocacode.com", password: "ghi789" },
+];
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  //check to see if email and password exists in db
+  //if they do, create and send back a token
+  //if they dont send back an error message
+  let user = users.find(
+    (user) => user.email === email && user.password === password
+  );
+  if (!user) {
+    res.status(401).send("Invalid email or password!");
+    return;
+  }
+  user.password = undefined; //removes password from the user object
+  const token = jwt.sign(user, mySecretKey, { expiresIn: "1hr" });
+  res.send(token);
+});
+
+app.post("/public", (req, res) => {
+  res.send("Welcome!");
+});
+
+app.get("/private", (req, res) => {
+  //lets requrire a valid token to see here
+  const token = req.headers.authorization || "";
+  if (!token) {
+    res.status(401).send("You must be logged in");
+    return;
+  }
+  jwt.verify(token, mySecretKey, (err, decoded) => {
+    if (err) {
+      res.status(401).send("You must be logged in");
+      return;
+    }
+    //HERE, we know that the token is valid
+    res.send(`Welcome ${decoded.email}!`);
+  });
+});
+
+export const api = functions.https.onRequest(app);
